b024d98c24d6436939347c5d208575e9f2d7a1d6
Kevin Torres <ktorres@creamas.org>
2022-08-22 12:58:34 -0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config({ path: '.env' });
 
-const puerto = process.env.DB_PUERTO;
+const puerto = process.env.DB_PUERTO || 5000;
 
 // Establecer un puerto
 app.listen(puerto, () => {
